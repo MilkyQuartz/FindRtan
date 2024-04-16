ada4cb1e03e63b30b5c5f65271661f46b8915880
Kimmothy <44524896+roekdk@users.noreply.github.com>
2024-04-17 08:26:23 +0900

    flip

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -12,6 +12,7 @@ public class Card : MonoBehaviour
     public SpriteRenderer frontImage;
     AudioSource audioSource;
     public AudioClip clip;
+        
 
     void Start()
     {
@@ -34,14 +35,17 @@ public class Card : MonoBehaviour
         if (GameManager.Instance.secondCard != null) return;
 
         audioSource.PlayOneShot(clip);
-        anim.SetBool("IsOpen", true);
-        front.SetActive(true);
-        back.SetActive(false);
+        anim.SetBool("IsOpen", true);       
+
+        //front.SetActive(true);
+        //back.SetActive(false);
+        
 
         // 1. firstCard가 비었다면 내 정보를 넘겨준다
         if (GameManager.Instance.firstCard == null)
         {
             GameManager.Instance.firstCard = this;
+            //Invoke("CloseCardInvoke", 3f); //3초후 카드닫기함수
         }
         else
         {
@@ -62,21 +66,33 @@ public class Card : MonoBehaviour
 
     public void CloseCard()
     {
-        Invoke("CloseCardInvoke", 0.5f);
+        Invoke("CloseCardInvoke", 0.8f);        
     }
     void CloseCardInvoke()
     {
-//어이쿠야
-//어머나
-//아이고 넘어졌네
-        anim.SetBool("IsOpen", false);
-        front.SetActive(false);
-        back.SetActive(true);
-    }
+        //anim.SetBool("IsOpen", false);
+        anim.SetBool("IsOpen", true);  
+        //front.SetActive(false);
+        //back.SetActive(true);
+    }   
 
     // hierarchy에 저장돼 있는 카드 뒷면의 색을 변경하는 작업
     public void ChangeColor(Color color)
     {
         back.GetComponent<SpriteRenderer>().color = color;
     }
+
+    public void ChangeCard()
+    {
+        if(!front.activeSelf){
+            front.SetActive(true);
+            back.SetActive(false);                              
+        }
+        else
+        {            
+            front.SetActive(false);
+            back.SetActive(true); 
+        }
+        anim.SetBool("IsOpen", false);   
+    }    
 }
